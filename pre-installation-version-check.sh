4579be3809f0794bd7cb87ea467956473ce219c3
Adam Fabian <adam.fabian@rackspace.com>
2026-03-10 18:01:25 -0500

    Add GC_INTERVAL check stuff to pre-installation-version-check.sh

--- pre-installation-version-check.sh
+++ pre-installation-version-check.sh
@@ -6,16 +6,19 @@ echo "CURRENT chart app version: $(helm -n kube-system list -o json | jq -r '.[]
 echo "CURRENT ENABLE_SSL: $(helm -n kube-system get values kube-ovn -o json  | jq .networking.ENABLE_SSL
 false)"
 echo "CURRENT IMAGE TAG: $(helm -n kube-system get values kube-ovn | gojq -r --yaml-input .global.images.kubeovn.tag)"
+echo "CURRENT GC_INTERVAL: $(helm -n kube-system get values kube-ovn | gojq -r --yaml-input .performance.GC_INTERVAL)"
 echo
 echo "BASE TARGET /opt/genestack overrides image: $(gojq -r --yaml-input .global.images.kubeovn.tag /opt/genestack/base-helm-configs/kube-ovn/kube-ovn-helm-overrides.yaml)"
 echo "BASE TARGET genestack helm-chart-version: $(gojq --yaml-input -r '.charts.["kube-ovn"]' /opt/genestack/helm-chart-versions.yaml)"
 echo "BASE TARGET ENABLE_SSL: $(gojq --yaml-input .networking.ENABLE_SSL /opt/genestack/base-helm-configs/kube-ovn/kube-ovn-helm-overrides.yaml)"
 echo "BASE IMAGE TAG: $(gojq -r --yaml-input .global.images.kubeovn.tag /opt/genestack/base-helm-configs/kube-ovn/kube-ovn-helm-overrides.yaml)"
 echo "BASE IMAGE REGISTRY: $(gojq -r --yaml-input .global.registry.address /opt/genestack/base-helm-configs/kube-ovn/kube-ovn-helm-overrides.yaml)"
+echo "BASE GC_INTERVAL: $(gojq -r --yaml-input .performance.GC_INTERVAL /opt/genestack/base-helm-configs/kube-ovn/kube-ovn-helm-overrides.yaml)"
 echo
 echo "ETC TARGET helm-chart-version: $(gojq --yaml-input -r '.charts.["kube-ovn"]' /etc/genestack/helm-chart-versions.yaml)"
 echo "ETC TARGET ENABLE_SSL: $(gojq --yaml-input .networking.ENABLE_SSL /etc/genestack/helm-configs/kube-ovn/kube-ovn-helm-overrides.yaml)"
 echo "ETC TARGET TAG: $(gojq -r --yaml-input .global.images.kubeovn.tag /etc/genestack/helm-configs/kube-ovn/kube-ovn-helm-overrides.yaml)"
+echo "ETC GC_INTERVAL: $(gojq -r --yaml-input .performance.GC_INTERVAL /etc/genestack/helm-configs/kube-ovn/kube-ovn-helm-overrides.yaml)"
 echo
 echo "ENVIRONMENT INSTALL TIMEOUT: $(awk '/timeout/ {print $2}' /opt/genestack/bin/install-kube-ovn.sh)"
 echo "Hyperconverged labs need endpoint http://neutron-server.openstack.svc.cluster.local:9696"
